fb11b3ad94acf07d9908a658cafc9843418a5de5
Joshua-Posadas <jposadas1@nait.ca>
2026-01-25 19:12:53 -0700

    Character Select implemented
    The title screen is finished and I'm done the character select screen for the most part but the scripts need to be fixed

---
+++ Assets/Scripts/WeaponSelectScreen.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class WeaponSelectScreen : MonoBehaviour
+{
+    public string[] customizationOptions;
+    public GameObject buttonPrefab;
+    public Transform parent;
+    public UnityEvent OnWeaponSelected;
+
+    private void Awake()
+    {
+        customizationOptions = System.Enum.GetNames(typeof(WeaponType));
+
+    }
+
+    public void InitializeWeaponSelectScreen()
+    {
+        foreach (string option in customizationOptions)
+        {
+            GameObject tmp = Instantiate(buttonPrefab, parent);
+            TMP_Text t = tmp.GetComponentInChildren<TMP_Text>();
+            t.text = option;
+
+            Button b = tmp.GetComponent<Button>();
+            b.onClick.AddListener(delegate { CharacterSelectSingleton.Instance.SetWeaponType(option); });
+            b.onClick.AddListener(delegate { OnWeaponSelected?.Invoke(); }
+                );
+        }
+    }
+}
